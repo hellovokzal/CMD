11d94d8498aed83056b467e7df5d4517b8eb480e
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-05-30 15:03:54 +0500

    Update Host.py

--- Host.py
+++ Host.py
@@ -3,17 +3,20 @@ import telebot
 bot = telebot.TeleBot("6219995606:AAG4jYJoXYOIMJuBoHDTQ9dU8X9IEjIU6xc")
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(messsge.chat.id, "Привет, напиши команду /help, чтобы узнать подробнее команд")
+    bot.send_message(message.chat.id, "Привет, напиши команду /help, чтобы узнать подробнее команд")
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, "Список команд:\n/check <твоя ссылка> - узнать работает ли интернет или нет")
 @bot.message_handler(commands=['check'])
 def check(message):
-    url = message.text[7:len(message.text)]
-    bot.send_message(message.chat.id, f"Проверяем ссылку {url}...")
     try:
-        link = r.get(url)
-        bot.send_message(message.chat.id, f"Ссылка работает!\nКод: {link.status_code}")
+        url = message.text[7:len(message.text)]
+        bot.send_message(message.chat.id, f"Проверяем ссылку {url}...")
+        try:
+            link = r.get(url)
+            bot.send_message(message.chat.id, f"Ссылка работает!\nКод: {link.status_code}")
+        except:
+            bot.send_message(message.chat.id, f"Ссылка не работает!\nКод: {link.status_code}")
     except:
-        bot.send_message(message.chat.id, f"Ссылка не работает!\nКод: {link.status_code}")
+        bot.send_message(message.chat.id, "Вы ввели неправильную ссылку!\nКод: unknown")
 bot.polling()
